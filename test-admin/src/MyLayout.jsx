bb6c0dc9cb9a531635478b4bef8ecabc9fb667ff
Arantza Parra <A01782023@tec.mx>
2023-09-07 13:09:34 -0600

    Hook logout

---
+++ test-admin/src/MyLayout.jsx
@@ -0,0 +1,35 @@
+
+import { forwardRef } from 'react';
+import { AppBar, Layout, UserMenu, useLogout } from 'react-admin';
+import { MenuItem } from '@mui/material';
+import ExitIcon from '@mui/icons-material/PowerSettingsNew';
+
+// It's important to pass the ref to allow Material UI to manage the keyboard navigation
+const MyLogoutButton = forwardRef((props, ref) => {
+    const logout = useLogout();
+    const handleClick = () => logout();
+    return (
+        <MenuItem
+            onClick={handleClick}
+            ref={ref}
+            // It's important to pass the props to allow Material UI to manage the keyboard navigation
+            {...props}
+        >
+            <ExitIcon/>  logout
+        </MenuItem>
+    );
+});
+
+const MyUserMenu = () => (
+    <UserMenu>
+        <MyLogoutButton />
+    </UserMenu>
+);
+
+const MyAppBar = () => <AppBar userMenu={<UserMenu />} />;
+
+const MyLayout = (props) => (
+    <Layout {...props} appBar={MyAppBar} />
+);
+
+export default MyLayout;
