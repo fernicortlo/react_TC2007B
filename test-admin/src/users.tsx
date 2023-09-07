22fd3355e17217cd5d586e32d0377007e759d923
fernicortlo <a01026613@tec.mx>
2023-09-07 16:34:05 -0600

    user unique

--- test-admin/src/users.tsx
+++ test-admin/src/users.tsx
@@ -4,7 +4,6 @@ import MyUrlField from './MyUrlField';
 
 export const UserList = () => {
     const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
-
     return (
         <List>
             {isSmall ? (
@@ -15,15 +14,15 @@ export const UserList = () => {
                 />
             ) : (
                 <Datagrid rowClick="show">
-                    <TextField source="id"  />
-                    <TextField source="Name" label="Nombre" />
-                -   <TextField source="username" label="Usuario" />
-                    <EmailField source="email" label="Email" />
-                -   <TextField source="address.street" label="Dirección de calle" />
-                    <TextField source="phone" label="Telefono" />
-                    <MyUrlField source="website"/>
-                    <TextField source="company.name" label="Nombre de la compañía" />
-                </Datagrid>
+            <TextField source="id"  />
+            <TextField source="Name" label="Nombre" />
+        -   <TextField source="username" label="Usuario" />
+            <EmailField source="email" label="Email" />
+        -   <TextField source="address.street" label="Dirección de calle" />
+            <TextField source="phone" label="Telefono" />
+            <MyUrlField source="website" label="Sitio WEB" />
+            <TextField source="company.name" label="Nombre de la compañía" />
+  </Datagrid>
             )}
         </List>
     );
