151e9bb6ade92aac4a9ba0d68c77abe94fd4e606
Lucia Barrenechea <lbarrenecheac@gmail.com>
2023-09-19 16:05:54 -0600

    new pull request

--- test-admin/src/App.tsx
+++ test-admin/src/App.tsx
@@ -9,10 +9,11 @@ import AlbumIcon from '@mui/icons-material/Album';
 import { Dashboard } from './Dashboard';
 import { authProvider } from './authProvider';
 import { i18nProvider } from './i18nProvider';
-
+import MyLayout  from './MyLayout';
+import MyLoginPage from "./MyLoginPage";
 
 export const App = () => (
-  <Admin authProvider={authProvider} dataProvider={dataProvider} dashboard={Dashboard} i18nProvider={i18nProvider}>
+  <Admin loginPage={MyLoginPage} layout={MyLayout} authProvider={authProvider} dataProvider={dataProvider} dashboard={Dashboard} i18nProvider={i18nProvider}>
         <Resource 
             name="posts"
             list={PostList}
