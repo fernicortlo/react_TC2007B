fc6ecd1a9d5ef45821d5227681b2aae96a5160a9
fernicortlo <a01026613@tec.mx>
2023-09-22 09:13:05 -0600

    ya funciona un test bitchesss!!!

--- test-admin/src/PostFilterSidebar.test.js
+++ test-admin/src/PostFilterSidebar.test.js
@@ -1,24 +1,44 @@
 // PostFilterSidebar.test.js
+
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import { PostFilterSidebar } from './posts.tsx'; // Adjust the import path
+
+// describe('PostFilterSidebar', () => {
+//   it('should render the PostFilterSidebar component', () => {
+//     const { getByText } = render(<PostFilterSidebar />);
+    
+//     // Use getByText or other queries to find elements in the rendered component
+//     const categoryLabel = getByText('Category');
+//     const idLabel = getByText('ID');
+//     const usersLabel = getByText('Users');
+//     const titleLabel = getByText('title');
+//     const bodyLabel = getByText('Body');
+    
+//     // Assert that the elements are present in the rendered component
+//     expect(categoryLabel).toBeInTheDocument();
+//     expect(idLabel).toBeInTheDocument();
+//     expect(usersLabel).toBeInTheDocument();
+//     expect(titleLabel).toBeInTheDocument();
+//     expect(bodyLabel).toBeInTheDocument();
+//   });
+// });
+
 import React from 'react';
 import { render } from '@testing-library/react';
-import { PostFilterSidebar } from './posts.tsx'; // Adjust the import path
+import { PostFilterSidebar } from './posts'; 
 
 describe('PostFilterSidebar', () => {
-  it('should render the PostFilterSidebar component', () => {
-    const { getByText } = render(<PostFilterSidebar />);
-    
-    // Use getByText or other queries to find elements in the rendered component
-    const categoryLabel = getByText('Category');
-    const idLabel = getByText('ID');
-    const usersLabel = getByText('Users');
-    const titleLabel = getByText('title');
-    const bodyLabel = getByText('Body');
-    
-    // Assert that the elements are present in the rendered component
-    expect(categoryLabel).toBeInTheDocument();
-    expect(idLabel).toBeInTheDocument();
-    expect(usersLabel).toBeInTheDocument();
-    expect(titleLabel).toBeInTheDocument();
-    expect(bodyLabel).toBeInTheDocument();
+  describe('Labels', () => {
+    it('should render necessary labels', () => {
+      const { getByText } = render(<PostFilterSidebar />);
+      
+      expect(getByText('Category')).toBeInTheDocument();
+      expect(getByText('ID')).toBeInTheDocument();
+      expect(getByText('Users')).toBeInTheDocument();
+      expect(getByText('title')).toBeInTheDocument();
+      expect(getByText('Body')).toBeInTheDocument();
+    });
   });
+
 });
