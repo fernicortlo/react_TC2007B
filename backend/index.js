1dc17c410f10b680a35d750c36f4713c364c3cf0
fernicortlo <a01026613@tec.mx>
2023-09-30 11:10:53 -0600

    ticket sup aula connect

--- backend/index.js
+++ backend/index.js
@@ -5,40 +5,52 @@ bodyParser=require('body-parser')
 const bcrypt=require("bcrypt")
 const jwt=require("jsonwebtoken")
 
+const uri = "mongodb+srv://Fer:1234@pruebas.1e0thqh.mongodb.net/pruebas";
 let db;
+ 
 const app=express();
 app.use(cors());
 app.use(bodyParser.json());
+const client = new MongoClient(uri);
 
 
 async function connectDB(){
-    let client=new MongoClient("mongodb://localhost:27017/ticketSystem")
-    await client.connect();
-    db=client.db();
-    console.log("conectado a la base de datos")
+    try {
+        await client.connect();
+        db = client.db();
+        console.log("Connected to the database");
+    } catch(err) {
+        console.error("Failed to connect to the database", err);
+    }
 }
 
+
 //getList, getMany, getManyReference
-app.get("/tickets", async (request, response)=>{
-    
-    if ("_sort" in request.query){
-        let sortBy=request.query._sort;
-        let sortOrder=request.query._order=="ASC"?1:-1;
-        let start=Number(request.query._start);
+app.get("/Tickets", async (request, response)=>{
+    // determinar donde esta el endpoint
+    if ("_sort" in request.query){ // list
+        let sortBy=request.query._sort; 
+        let sortOrder=request.query._order=="ASC"?1:-1; // si es asendente, 1, si no, -1 (descendente)
+        let start=Number(request.query._start); 
         let end=Number(request.query._end);
-        let sorter={}
-        sorter[sortBy]=sortOrder
-        let data=await db.collection('Tickets').find({}).sort(sorter).project({_id:0}).toArray();
+        let sorter={} // dado que no puedo utilizar una variable como nombre o llave, se agreaga así
+        sorter[sortBy]=sortOrder // se agrega la llave y el valor: asc: 1, desc: -1
+        let data=await db.collection('Tickets').find({}).sort(sorter).project({_id:0}).toArray(); 
         response.set('Access-Control-Expose-Headers', 'X-Total-Count')
         response.set('X-Total-Count', data.length)
         data=data.slice(start, end)
         console.log(data)
         response.json(data)
     }
+    else {
+        let data = await db.collection('Tickets').find({}).project({_id:0}).toArray();
+        response.json(data);
+    }
+    
 })
 
 //getOne
-app.get("/tickets/:id", async (request, response)=>{
+app.get("/Tickets/:id", async (request, response)=>{
     let data=await db.collection('Tickets').find({"id": Number(request.params.id)}).project({_id:0}).toArray();
     response.json(data[0]);
 })
