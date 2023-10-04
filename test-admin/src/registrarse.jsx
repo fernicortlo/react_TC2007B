43c984365f11ad00df69f24ea4f34af73bc6beeb
Natalia Valles Villegas <nataliavallesv@gmail.com>
2023-10-03 18:38:14 -0600

    registrarseultimo

--- test-admin/src/registrarse.jsx
+++ test-admin/src/registrarse.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { SimpleForm, TextInput, Button, SelectInput, choices } from 'react-admin';
+import { SimpleForm, TextInput, Button, SelectInput, choices, Create, useCreate } from 'react-admin';
 import { rolChoices } from "./choices";
 
 const Registrarse = () => {
@@ -56,18 +56,85 @@ const Registrarse = () => {
     return (
         <div>
             <h2>Registro de nuevos usuarios</h2>
-            <SimpleForm>
-            <TextInput source="correo" label="Correo" name="correo" onChange={handleChange} />
-            <TextInput source="pass" label="Contraseña" type="password" name="pass" onChange={handleChange} />
-            <TextInput source="nombreCompleto" label="Nombre Completo" name="nombreCompleto" onChange={handleChange} />
-            <SelectInput source="rol" label="Rol" name="rol" choices={rolChoices} onChange={handleChange} />
-            <TextInput source="nombreAula" label="Nombre de Aula" name="aula.nombreAula" onChange={handleChange} />
-            <TextInput source="lugarAula" label="Lugar de Aula" name="aula.lugarAula" onChange={handleChange} />
-            <TextInput source="sponsorAula" label="Patrocinador de Aula" name="aula.sponsorAula" onChange={handleChange} />
+            
+            
+            <SimpleForm
+            save={handleSendData}>
+            <TextInput source="correo" label="Correo" name="correo" onChange={handleChange} required />
+            <TextInput source="pass" label="Contraseña" type="password" name="pass" onChange={handleChange} required/>
+            <TextInput source="nombreCompleto" label="Nombre Completo" name="nombreCompleto" onChange={handleChange} required/>
+            <SelectInput source="rol" label="Rol" name="rol" choices={rolChoices} onChange={handleChange} required/>
+            <TextInput source="nombreAula" label="Nombre de Aula" name="aula.nombreAula" onChange={handleChange} required/>
+            <TextInput source="lugarAula" label="Lugar de Aula" name="aula.lugarAula" onChange={handleChange} required/>
+            <TextInput source="sponsorAula" label="Patrocinador de Aula" name="aula.sponsorAula" onChange={handleChange} required />
             <Button label="Crear Usuario" onClick={handleSendData} />
             </SimpleForm>
+            
         </div>
     );
 };
 
 export default Registrarse;
+
+
+// import { useNotify, useRecordContext} from "react-admin";
+// import { Card, CardContent } from '@mui/material';
+// import CategoryIcon from '@mui/icons-material/LocalOffer';
+// import {
+//     List,
+//     Datagrid,
+//     TextField,
+//     // ReferenceField,
+//     EditButton,
+//     // Edit,
+//     Create,
+//     SimpleForm,
+//     ReferenceInput,
+//     TextInput,
+//     useRefresh,
+//     useRedirect,
+//     // FilterList,
+//     // FilterListItem,
+//     // FilterLiveSearch,
+//     // Button,
+//     // useCreate,
+//     SelectInput
+// } from "react-admin";
+// import React, { useState, ChangeEvent } from 'react';
+// import { choices, useCreate } from 'react-admin';
+// import { rolChoices } from "./choices";
+
+// const registrarse = () => {
+//     const notify= useNotify();
+//     const refresh = useRefresh();
+//     const redirect = useRedirect();
+    
+
+    // const [tipoChoices, setTipoChoices] = useState<ChoiceOption[]>([]);
+
+    // const handleClasificacionChange = (event: React.ChangeEvent<{ name?: string; value: any }>) => {
+    //     const selectedClasificacion = event.target.value as string;
+    //     setTipoChoices(tipoChoicesMapping[selectedClasificacion] || []);
+    // };
+    
+//     const onSuccess = () => {
+//         notify('Usuario Creado', {undoable: true});
+//         redirect('/login');
+//         refresh();
+//     };
+
+//     return (
+//         <Create mutationOptions={{ onSuccess }}>
+//             <SimpleForm>
+//             <TextInput source="correo" label="Correo" name="correo" onChange={handleChange} required />
+//              <TextInput source="pass" label="Contraseña" type="password" name="pass" onChange={handleChange} required/>
+//              <TextInput source="nombreCompleto" label="Nombre Completo" name="nombreCompleto" onChange={handleChange} required/>
+//              <SelectInput source="rol" label="Rol" name="rol" choices={rolChoices} onChange={handleChange} required/>
+//              <TextInput source="nombreAula" label="Nombre de Aula" name="aula.nombreAula" onChange={handleChange} required/>
+//              <TextInput source="lugarAula" label="Lugar de Aula" name="aula.lugarAula" onChange={handleChange} required/>
+//              <TextInput source="sponsorAula" label="Patrocinador de Aula" name="aula.sponsorAula" onChange={handleChange} required />
+//             </SimpleForm>
+//         </Create>
+//     );
+// };
+// export default registrarse;
