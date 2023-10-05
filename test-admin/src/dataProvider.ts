f9c26d23b7b9404f2b2cee6f41fdee43f2edded7
fernicortlo <a01026613@tec.mx>
2023-10-05 12:37:13 -0600

    adds supervisor de aula authentication

--- test-admin/src/dataProvider.ts
+++ test-admin/src/dataProvider.ts
@@ -1,3 +1,17 @@
-import jsonServerProvider from 'ra-data-json-server';
+import { fetchUtils } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
 
-export const dataProvider = jsonServerProvider("http://127.0.0.1:1337");
+interface Options {
+    headers?: HeadersInit;
+}
+
+const fetchJsonUtil = (url: string, options: Options = {}) => {
+    if (!options.headers) {
+        options.headers = new Headers({ Accept: "application/json" });
+    }
+    (options.headers as Headers).set("Authentication", localStorage.getItem("auth"));
+    return fetchUtils.fetchJson(url, options);
+};
+
+
+export const dataProvider = jsonServerProvider("http://127.0.0.1:1337", fetchJsonUtil);
